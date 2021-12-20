a8d89337550cf568ec299b03a2f8cd7b2b78742e
SnmLogic <snmlogic111@gmail.com>
2021-12-20 09:47:35 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Nav Module
 
-Initial release of the module.
+## _The solution for voice related stuff in Python_
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+Nav is a Python module which simplifies voice related stuff in Python.
+
+- Just import the Module class from the nav module
+- Use only one line of code to use the Text-To-Speech and speech_recognition.
+- ✨Magic ✨
+
+## Usage
+
+- Make a `.py` file.
+- Import Module class from the nav module. `from nav import Module`
+- Use `Module.Voice.speak("Your text here", "voice_gender")` to use the Text-To-Speech (TTS)
+- Use `Module.Voice.listen.said_text()` to use the speech_recognition.
+- You can use `Module.Music.play_music(path)` to play a music and `Module.Music.beep(hz, ms)` to make a beep sound.
+
+## Example (TTS)
+
+```
+from nav import Module
+
+my_text = "Your text here"
+Module.Voice.speak(my_text, "male")
+```
+
+The Nav module makes the life of programmers using voice related stuff in Python.
--- setup.py
+++ setup.py
@@ -1,32 +1,25 @@
 from distutils.core import setup
 setup(
-    name='nav',         # How you named your package folder (MyLib)
-    packages=['nav'],   # Chose the same as "name"
-    version='0.1',      # Start with a small number and increase it with every change you make
-    # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    name='nav',
+    packages=['nav'],
+    version='0.1',
     license='MIT',
-    # Give a short description about your library
     description='A module which simplifies stuff related to voice',
-    author='Navaneeth K',                   # Type in your name
-    author_email='snmlogic111@gmail.com',      # Type in your E-Mail
-    # Provide either the link to your github or to your website
+    author='Navaneeth K',
+    author_email='snmlogic111@gmail.com',
     url='https://github.com/SnmLogic/nav',
-    # I explain this later on
-    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
-    # Keywords that define your package best
-    keywords=['SOME', 'MEANINGFULL', 'KEYWORDS'],
-    install_requires=[            # I get to this in a second
-        'validators',
-        'beautifulsoup4',
+    download_url='https://github.com/SnmLogic/nav/archive/refs/tags/version_0.1.tar.gz',
+    keywords=['Voice', 'Nav', 'TTS'],
+    install_requires=[
+        'pyttsx3',
+        'speech_recognition',
+        'pyaudio'
     ],
     classifiers=[
-        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Development Status :: 3 - Alpha',
-        # Define that your audience are developers
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: MIT License',   # Again, pick a license
-        # Specify which pyhton versions that you want to support
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
