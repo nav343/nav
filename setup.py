fa6a47623bfb81e768c5ce2bc992bc81e98c5427
SnmLogic <snmlogic111@gmail.com>
2021-12-20 10:25:16 +0530

    Initial commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='nav_snmlogic',
     packages=['nav_tts'],
-    version='0.1',
+    version='0.2',
     license='MIT',
     description='A module which simplifies stuff related to voice',
     author='Navaneeth K',
